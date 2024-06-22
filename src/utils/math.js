45b040dd0e622ea1fe18c1f51ea7fff19121c006
JoaoDiogoCorrea <joaodiogo@gmail.com>
2024-06-22 13:26:52 -0300

    math tests

--- src/utils/math.js
+++ src/utils/math.js
@@ -1,18 +1,23 @@
 
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
+  if (typeof a !== 'number' || typeof b !== 'number')
     throw new Error('Both arguments must be numbers');
-  }
+
   return a + b;
 }
 
 function sumValues(obj) {
-  if (obj.leght === 0) {
-    throw new Error('The object is empty');
-  }
+
+  if (obj === undefined || obj.length === 0)
+    throw new Error('The object is undefined or is empty');
+
+  if (obj.every(i => typeof i === "number") === false)
+    throw new Error('In the object there is string');
+
   const value = obj.reduce((result, amount) => {
     return (result + amount);
   }, 0);
+
   return value
 }
 
--- tests/math.test.js
+++ tests/math.test.js
@@ -5,7 +5,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('sum the values', () => {
-  expect(sumValues([10,20,30,40])).toBe(100)
+  expect(sumValues([10, 20, 30, 40])).toBe(100)
 })
 
 test('throws error when first argument is not a number', () => {
@@ -14,4 +14,16 @@ test('throws error when first argument is not a number', () => {
 
 test('throws error when first argument is not a number', () => {
   expect(() => sum('1', undefined)).toThrow('Both arguments must be numbers');
+});
+
+test('throws error when the object is undefined', () => {
+  expect(() => sumValues()).toThrow('The object is undefined or is empty');
+});
+
+test('throws error when the object is empty', () => {
+  expect(() => sumValues([])).toThrow('The object is undefined or is empty');
+});
+
+test('throws error when in the object there is string', () => {
+  expect(() => sumValues([1, 12, '3'])).toThrow('In the object there is string');
 });
