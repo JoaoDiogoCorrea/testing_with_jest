243e876c4b80e116eec34237b15b9548f207de1d
JoaoDiogoCorrea <joaodiogo@gmail.com>
2024-06-25 21:14:34 -0300

    update ProductSchema

--- src/models/product.js
+++ src/models/product.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { 
+    type: String, 
+    required: true,
+    validate: {
+      validator: function(v) {
+        return v.length > 3;
+      },
+      message: props => `${props.value} é muito curto! Nome precisa ser maior que 3 caracteres.`
+    }
+  },
   price: { 
     type: Number, 
     required: true,
@@ -9,7 +18,7 @@ const ProductSchema = new mongoose.Schema({
       validator: function(v) {
         return v >= 0;
       },
-      message: props => `${props.value} is not a valid price! Price must be a positive number.`
+      message: props => `${props.value} não é um preço válido! O preço precisa ser positivo.`
     }
   },
   description: { type: String },
